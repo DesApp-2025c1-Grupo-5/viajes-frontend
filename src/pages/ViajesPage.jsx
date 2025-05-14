365406ced1ff87e266c3639f84afe7c9b0dd5235
GomezLau <lautaroezequiel.gomez@estudiantes.unahur.edu.ar>
2025-05-14 00:00:16 -0300

    Agregado de Tabla a las pagina de Viajes
    Diseño del  componente CardButton y Tabla para ViajesPage y futuras paginas que lo implementen.
    
    Co-Authored-By: Gaston Ezequiel Arevalo <gastonezequielarevalo53@gmail.com>
    Co-Authored-By: Giovanni Martini <111198294+Giovanni-Martini@users.noreply.github.com>

--- src/pages/ViajesPage.jsx
+++ src/pages/ViajesPage.jsx
@@ -1,11 +1,48 @@
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import Title from "../components/Title";
+//import Card from "../components/Card"
+import Tabla from "../components/Tabla";
+import { useEffect, useState } from "react";
+
+const tablaViajes = [{
+    id: 1234,
+    origen: "Deposito Central",
+    destino: "Deposito Norte",
+    fecha_salida: "2023-04-12",
+    fecha_llegada: "2023-04-25",
+    vehiculo: "ABC-123"
+},
+{
+    id: 1235,
+    origen: "Deposito Sur",
+    destino: "Deposito Central",
+    fecha_salida: "2023-04-08",
+    fecha_llegada: "2023-04-09",
+    vehiculo: "GHI-789"
+},
+{
+    id: 1236,
+    origen: "Deposito Norte",
+    destino: "Deposito Sur",
+    fecha_salida: "2023-04-15",
+    fecha_llegada: "-",
+    vehiculo: "DEF-456"
+},]
 
 const ViajesPage = () => {
      return (
         <>
         <Header></Header>
-        <NavBar></NavBar>
+        <div className = "flex">
+        <div>
+            <NavBar />
+        </div>
+        <div className="flex-1 p-6">
+            <Title color="text-pink-600" title="Viajes" description="Gestiona los viajes entre depósitos"></Title>
+            <Tabla viajes={tablaViajes} />
+        </div>
+      </div>
         </>
     );
 }
