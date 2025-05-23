3fb1cd051ccffeeada75b1778e82b197cdc2788d
Giovanni Martini <111198294+Giovanni-Martini@users.noreply.github.com>
2025-05-23 19:07:22 -0300

    Revert "Crear nuevos"

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,30 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-import VehiculosPage from "./pages/VehiculosPage";
-import ChoferesPage from "./pages/ChoferesPage";
-import DepositosPage from "./pages/DepositosPage";
-import TransportistasPage from "./pages/TransportistasPage";
-import ViajesPage from "./pages/ViajesPage";
-import DashboardPage from "./pages/DashboardPage";
-import NuevoChoferPage from "./pages/nuevoPages/NuevoChoferPage";
-import NuevoDepositoPage from "./pages/nuevoPages/NuevoDepositoPage";
-import NuevoTransportistaPage from "./pages/nuevoPages/NuevoTransportistaPage";
-import NuevoVehiculoPage from "./pages/nuevoPages/NuevoVehiculoPage";
-import NuevoViajePage from "./pages/nuevoPages/NuevoViajePage";
+import { FilmsPage } from "./pages/FilmsPage";
+// import { FilmsPage } from "./pages/FilmsPageReduxStyle";
+import { ActorsPage } from "./pages/ActorsPage";
+import { Box } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 export function AppRouter() {
   return (
     <Routes>
-      <Route path="/vehiculos" element={<VehiculosPage />} />
-      <Route path="/choferes" element={<ChoferesPage />} />
-      <Route path="/depositos" element={<DepositosPage />} />
-      <Route path="/transportistas" element={<TransportistasPage />} />
-      <Route path="/viajes" element={<ViajesPage />} />
-      <Route path="/nuevoChofer" element={<NuevoChoferPage />} />
-      <Route path="/nuevoDeposito" element={<NuevoDepositoPage />} />
-      <Route path="/nuevoTransportista" element={<NuevoTransportistaPage />} />
-      <Route path="/nuevoVehiculo" element={<NuevoVehiculoPage />} />
-      <Route path="/nuevoViaje" element={<NuevoViajePage />} />
-      <Route path="/" element={<DashboardPage />} />
+      <Route path='/films' element={<FilmsPage />} />
+      <Route path='/actors' element={<ActorsPage />} />
+      <Route path='/' element={
+        <Box sx={{ typography: 'h5', color: grey[900] }}>
+          Elija una opción del menú
+        </Box>
+      } />
     </Routes>
   );
 }
