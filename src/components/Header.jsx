e44996525ce64267b0d5eb17d60f953e127f283b
GeraDV <gerardodamian.velazquez@estudiantes.unahur.edu.ar>
2025-05-25 14:29:56 -0300

    Se agregó degradado al Título del Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     // falta aplicar el degradado
     <header className="g-whiteb-600 px-6 py-4 shadow-md">
       <Link to="/">
-        <h1 className="text-3xl text-blue-600 font-bold cursor-pointer">
+        <h1 className="text-3xl font-bold inline-block bg-[linear-gradient(to_right,_#1A56FF,_#00907C)] bg-clip-text text-transparent cursor-pointer">
             Logística Acme SRL
         </h1>
       </Link>
