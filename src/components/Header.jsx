f1daa152aa18fa322e58f85c2e6989d2deee1be2
GeraDV <gerardodamian.velazquez@estudiantes.unahur.edu.ar>
2025-05-25 14:24:12 -0300

    Se agregó redirección al Dashboard al hacer click sobre el título del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
+
   return (
     // falta aplicar el degradado
     <header className="g-whiteb-600 px-6 py-4 shadow-md">
-      <div>
-        <h1 className="text-3xl text-blue-600 font-bold cursor-pointer">Logística Acme SRL</h1>
-      </div>
+      <Link to="/">
+        <h1 className="text-3xl text-blue-600 font-bold cursor-pointer">
+            Logística Acme SRL
+        </h1>
+      </Link>
     </header>
   );
 };
