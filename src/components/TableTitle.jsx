8c2e7e8a20976e547da019c8b06052fd4d99e817
GeraDV <gerardodamian.velazquez@estudiantes.unahur.edu.ar>
2025-05-25 16:38:46 -0300

    Se corrigió el margen en TableTitle

--- src/components/TableTitle.jsx
+++ src/components/TableTitle.jsx
@@ -1,6 +1,6 @@
 const TableTitle = (props) => {
   return (
-    <div>
+    <div className="mb-6">
         <h1 className={`mt-8 text-lg font-bold ${props.color}`}>{props.title}</h1>
         <h2 className="text-md text-gray-500">{props.description}</h2>
     </div>
