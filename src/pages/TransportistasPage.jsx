3de381678ba9069a81747b213d1686d17ebd664e
GeraDV <gerardodamian.velazquez@estudiantes.unahur.edu.ar>
2025-06-01 03:12:31 -0300

    Se agregó filtrado por búsqueda a Empresas Transportistas

--- src/pages/TransportistasPage.jsx
+++ src/pages/TransportistasPage.jsx
@@ -8,32 +8,11 @@ import New from "../components/New";
 import { useEffect, useState } from "react";
 import transportistaService from "../services/EmpresasTransportistasService";
 
-// const tablatransportistas = [
-//   {
-//     razon_social: "Logística del Sur S.A.",
-//     cuit: "30-70985643-2",
-//     domicilio_fiscal: "Av. San Martín 4521, Rosario, Santa Fe",
-//     telefono: "+54 341 456-7890",
-//     email: "contacto@logisticasur.com.ar",
-//   },
-//   {
-//     razon_social: "Alimentos Naturales SRL",
-//     cuit: "33-60598421-9",
-//     domicilio_fiscal: "Calle Belgrano 123, Córdoba Capital, Córdoba",
-//     telefono: "+54 351 432-1098",
-//     email: "info@alimentosnaturales.com",
-//   },
-//   {
-//     razon_social: "Grupo TecnoData S.A.",
-//     cuit: "30-67891234-5",
-//     domicilio_fiscal: "Av. Corrientes 987, CABA",
-//     telefono: "+54 11 4455-8899",
-//     email: "soporte@tecnodata.com.ar",
-//   },
-// ];
 
 const TransportistasPage = () => {
   const [transportistas, setTransportistas] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const [transportistasFiltrado, setTransportistasFiltrado] = useState([]);
 
   useEffect(() => {
     const obtenerTransportistas = async () => {
@@ -44,10 +23,17 @@ const TransportistasPage = () => {
         console.log(error);
       }
     };
-
     obtenerTransportistas();
-
   }, []);
+
+  useEffect(()=>{
+    const resultadoFiltro = transportistas.filter(texto => 
+      `${texto.razon_social} ${texto.cuit_rut} ${texto.domicilio_fiscal} ${texto.email} ${texto.telefono}`
+      .toLowerCase()
+      .includes(busqueda)
+    );
+    setTransportistasFiltrado(resultadoFiltro);
+  }, [busqueda, transportistas]);
 
   return (
     <>
@@ -75,8 +61,8 @@ const TransportistasPage = () => {
                 colorHover="hover:bg-purple-500"
               ></New>
             </div>
-            <SearchBar></SearchBar>
-            <TablaTransportistas transportistas={transportistas} />
+            <SearchBar onSearch={setBusqueda}/>
+            <TablaTransportistas transportistas={transportistasFiltrado} />
           </div>
         </div>
       </div>
