71eab4fae05592fe2f33c79ee010d55fd900cc35
Giovanni-Martini <giovanni.martini@estudiantes.unahur.edu.ar>
2025-07-11 14:04:11 -0300

    Cambiado un nombre.

--- src/pages/editPages/EdithTransportista.jsx
+++ src/pages/editPages/EdithTransportista.jsx
@@ -116,7 +116,7 @@ const EditarTransportistasPage = () => {
             <Input
               value={cuit_rut}
               onChange={(e) => setCuitRut(e.target.value)}
-              title="CuitRut"
+              title="CUIT/RUT"
               id="idCuitRut"
               type="number"
               required
